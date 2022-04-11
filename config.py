89d3a13570eaf221ed7909a6dd50f3641659b719
Artur321710 <60099333+Artur321710@users.noreply.github.com>
2022-04-11 11:40:05 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 BOT_TOKEN = '5255116456:AAFn5qneT_KJA9XBwHFPeCuMShyaFu7WfyA'
 OWNER_TAG = '@disson_1'
-CHANEL_LINK = 't.me/telegram'
+CHANEL_LINK = 'https://t.me/+0MDxJYlxYRM3MzBi'
 tokenusr = ''  # Токен киви
 phoneusr = '+79995888334'  # Номер киви киви
 price = 100  # Цена подписки
